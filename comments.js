bdfbc96c8effa123975dd79e6fae9b927fea6f1e
Dominik Zając <zajac_dominik@onet.pl>
2019-01-12 09:09:54 +0100

    Update comments.js (add edit_comment, thumb_up, thumb_down)

--- comments.js
+++ comments.js
@@ -21,9 +21,33 @@ function comments(state = [], action) {
                 votes: 0
             }, ...state.comments];
         
+        case EDIT_COMMENT:
+            return state.map(comment => {
+                if(comment.id === action.id) {
+                    return{...comment, text: action.id}
+                }
+                return comment;
+            });
+
         case DELETE_COMMENT:
             return state.filter(comment => comment.id !== action.id);
         
+        case THUMB_UP_COMMENT:
+            return state.map(comment => {
+                if(comment.id === action.id) {
+                    return {...comment, votes: comment.votes + 1}
+                }
+                return comment;
+            });
+
+        case THUMB_DOWN_COMMENT:
+            return state.map(comment =>{
+                if(comment.id === action.id) {
+                    return {...comment, votes: comment.votes - 1}
+                }
+                return comment;
+            }); 
+
         default:
             return state
     }
